375c18b9cf2def5bb4da6f5eeb1f05cba5e15f95
Ariiellus <arielcardenas95@hotmail.com>
2024-12-18 14:02:26 -0600

    Fund Me section. Test 2.

--- SSCD/Fund Me/FundMe.sol
+++ SSCD/Fund Me/FundMe.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {PriceConverter} from "./PriceConverter.sol";
+
 contract FundMe {
-    uint256 public minUsd = 5;
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 5e18;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     // ETH Price feed = 0x689B1d8FB0c64ACFEeFA6BdE1d31f215e92B6fd4
 
     function fund() public payable {
-                
-        require(msg.value > minUsd, "You need to send more ETH"); 
+        require(msg.value.getConversionRate() > minimumUsd, "You need to send more ETH"); // 1^18 = 1 ETH
         
         // 1^18 = 1 ETH
         // 1^12 = 1 Gwei
@@ -16,9 +22,17 @@ contract FundMe {
         // If msg.value < minUsd, the transaction will revert
         // Revert = Undo any actions that have been done and sent the remaining gas back
 
-
-
+        funders.push(msg.sender); 
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
-    function withdraw() public {}
+         function withdraw() public {
+        // for loop
+        //[1, 2, 3, 4] -> Elements
+        // 0, 1, 2, 3 -> Indexex
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder =funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+    }
 }
