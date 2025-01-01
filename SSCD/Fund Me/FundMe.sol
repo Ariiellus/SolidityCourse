4511be9326216556365f5b759a9b717c8641ac53
Ariiellus <arielcardenas95@hotmail.com>
2025-01-01 10:00:17 -0600

    First steps in Foundry

--- SSCD/Fund Me/FundMe.sol
+++ SSCD/Fund Me/FundMe.sol
@@ -7,77 +7,93 @@ error notOwner();
 error callFailed();
 
 contract FundMe {
-  using PriceConverter for uint256;
+    using PriceConverter for uint256;
 
-  // wo constant 783,228 gas
-  // w constant 762,891 gas
+    // wo constant 783,228 gas
+    // w constant 762,891 gas
 
-  uint256 public constant MINIMUM_USD = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
-  address[] public funders;
-  mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded)
+        public addressToAmountFunded;
 
-  // ETH Price feed = 0x689B1d8FB0c64ACFEeFA6BdE1d31f215e92B6fd4
+    // ETH Price feed = 0x689B1d8FB0c64ACFEeFA6BdE1d31f215e92B6fd4
 
-  address public immutable i_owner;
-  // wo immutable 762,903 gas
-  // w immutable 739,720 gas
-  constructor() {
-    i_owner = msg.sender;
-  }
-
-  function fund() public payable {
-      require(msg.value.getConversionRate() > MINIMUM_USD, "You need to send more ETH"); // 1^18 = 1 ETH
-      
-      // 1^18 = 1 ETH
-      // 1^12 = 1 Gwei
-
-      // If msg.value < minUsd, the transaction will revert
-      // Revert = Undo any actions that have been done and sent the remaining gas back
-
-      funders.push(msg.sender); 
-      addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
-  }
-
-  function withdraw() public onlyOwner {      
-    // for loop
-    //[1, 2, 3, 4] -> Elements
-    // 0, 1, 2, 3 -> Indexex
-    for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
-        address funder =funders[funderIndex];
-        addressToAmountFunded[funder] = 0;
+    address public immutable i_owner;
+    // wo immutable 762,903 gas
+    // w immutable 739,720 gas
+    constructor() {
+        i_owner = msg.sender;
     }
 
-    // reset the array
-    funders = new address[](0);
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() > MINIMUM_USD,
+            "You need to send more ETH"
+        ); // 1^18 = 1 ETH
 
-    // transfer, if this fails throw error, capped at 2300 gas
-    // payable(msg.sender).transfer(address(this).balance);
+        // 1^18 = 1 ETH
+        // 1^12 = 1 Gwei
 
-    // send, if this fails returns boolean, capped at 2300 gas
-    // bool = sendSuccess = payable(msg.sender).send(address(this).balance);
-    // require(sendSuccess, "Send Failed");
+        // If msg.value < minUsd, the transaction will revert
+        // Revert = Undo any actions that have been done and sent the remaining gas back
 
-    // call, if this fails returns bool
-    (bool callSuccess, ) = payable(msg.sender).call{value : address(this).balance}("");
-    // require(callSuccess, "Call Failed");
-    if(callSuccess) {revert callFailed();}
-  }
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] =
+            addressToAmountFunded[msg.sender] +
+            msg.value;
+    }
 
-  modifier onlyOwner(){
-    // require(msg.sender == i_owner, "Sender is not owner!");
+    function withdraw() public onlyOwner {
+        // for loop
+        //[1, 2, 3, 4] -> Elements
+        // 0, 1, 2, 3 -> Indexex
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
 
-    // wo revert error 739,720 gas
-    // w revert error 715,431 gas
-    if(msg.sender != i_owner) {revert notOwner();}
-    _;
-  }
+        // reset the array
+        funders = new address[](0);
 
-  receive() external payable {
-    fund();
-  }
+        // transfer, if this fails throw error, capped at 2300 gas
+        // payable(msg.sender).transfer(address(this).balance);
 
-  fallback() external payable { 
-    fund();
-  }
+        // send, if this fails returns boolean, capped at 2300 gas
+        // bool = sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Failed");
+
+        // call, if this fails returns bool
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        // require(callSuccess, "Call Failed");
+        if (callSuccess) {
+            revert callFailed();
+        }
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "Sender is not owner!");
+
+        // wo revert error 739,720 gas
+        // w revert error 715,431 gas
+        if (msg.sender != i_owner) {
+            revert notOwner();
+        }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }
--- readme.md
+++ readme.md
@@ -1,30 +1,35 @@
-## About this repo
+# About this repo
 
 This repo contains my progress taking the different courses from [Cyfrin Updraft](https://x.com/CyfrinUpdraft)
 
 ## Courses
 
-This part contains my progress taking the Intermediate Course "[Foundry Fundamentals](https://updraft.cyfrin.io/courses/foundry/)" 
+This part contains my progress taking the Intermediate Course "[Foundry Fundamentals](https://updraft.cyfrin.io/courses/foundry/)"
 
 ### Section 1: Foundry Simple Storage
+
 Started on: 01/01/2025
-Finished on: 
+Finished on:
 
 This part contains my progress taking the Beginner Course "[Solidity Smart Contract Development](https://updraft.cyfrin.io/courses/solidity)"
 
 ### Section 4: AI Prompting
+
 Started on: 31/12/2024
 Finished on: 31/12/2024
 
 ### Section 3: Fund Me
+
 Started on: 02/12/2024
 Finished on: 31/12/2024
 
 ### Section 2: Storage Factory
+
 Started on: 30/11/2024
 Finished on: 30/11/2024
 
 ### Section 1: Simple Storage
+
 Started on: 23/11/2024
 Finished on: 26/11/2024
 
