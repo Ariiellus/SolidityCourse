3f9cdca05675a32a2b2061f212365319a340ebab
Ariiellus <arielcardenas95@hotmail.com>
2025-06-18 14:57:31 -0600

    Agrego carpetas de retos como parte del repo principal

---
+++ speedrunEthereum/challenge-1-decentralized-staking/packages/hardhat/contracts/ExampleExternalContract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
+
+contract ExampleExternalContract {
+    bool public completed;
+
+    function complete() public payable {
+        completed = true;
+    }
+
+    function getCompleted() public view returns (bool) {
+        return completed;
+    }
+}
