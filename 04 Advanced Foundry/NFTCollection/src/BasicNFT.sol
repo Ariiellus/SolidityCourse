16666b5d73e1eb380c8343d12818daac685e9bd9
Ariiellus <arielcardenas95@hotmail.com>
2025-07-14 19:00:20 -0600

    NFT Deployed

--- 04 Advanced Foundry/NFTCollection/src/BasicNFT.sol
+++ 04 Advanced Foundry/NFTCollection/src/BasicNFT.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+// @title BasicNFT
+// @author @Ariiellus
+// @notice This is a basic NFT contract from the Cyfrin Advanced Foundry Course
+
 contract BasicNFT is ERC721 {
     uint256 private s_tokenCounter;
     mapping(uint256 => string) private s_tokenURIs;
