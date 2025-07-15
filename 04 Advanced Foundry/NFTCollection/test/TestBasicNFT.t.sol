806c8ca4b1da6f8654ebf8d891d626b549e9c4ca
Ariiellus <arielcardenas95@hotmail.com>
2025-07-14 18:42:07 -0600

    NFT deployed

--- 04 Advanced Foundry/NFTCollection/test/TestBasicNFT.t.sol
+++ 04 Advanced Foundry/NFTCollection/test/TestBasicNFT.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
-import {DeployBasicNFT} from "../script/DeployBasicNFT.sol";
+import {DeployBasicNFT} from "../script/DeployBasicNFT.s.sol";
 
 contract TestBasicNFT is Test {
   DeployBasicNFT public deployer;
