7329a928f9dc584f0eb576e97fe9020c7b2b5f0c
Ariiellus <arielcardenas95@hotmail.com>
2025-12-16 10:40:17 -0600

    merkle airdrop test done

--- 04 Advanced Foundry/MerkleAirdrop/src/MerkleAirdrop.sol
+++ 04 Advanced Foundry/MerkleAirdrop/src/MerkleAirdrop.sol
@@ -32,7 +32,7 @@ contract MerkleAirdrop {
         }
         // hashing twice to avoid collision
         bytes32 leaf = keccak256(
-            bytes.concat(keccak256(abi.encodePacked(account, amount)))
+            bytes.concat(keccak256(abi.encode(account, amount)))
         );
         if (!MerkleProof.verify(merkleProof, I_MERKLE_ROOT, leaf)) {
             revert MerkleAirdrop__InvalidProof();
