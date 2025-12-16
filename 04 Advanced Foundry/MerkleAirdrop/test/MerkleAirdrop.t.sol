cbf52467aac3c5b32d55520d2e8978e1b4986e36
Ariiellus <arielcardenas95@hotmail.com>
2025-12-16 11:40:17 -0600

    adding deployment script

--- 04 Advanced Foundry/MerkleAirdrop/test/MerkleAirdrop.t.sol
+++ 04 Advanced Foundry/MerkleAirdrop/test/MerkleAirdrop.t.sol
@@ -8,29 +8,29 @@ import {BagelToken} from "../src/BagelToken.sol";
 contract MerkleAirdropTest is Test {
     MerkleAirdrop public airdrop;
     BagelToken public token;
-    bytes32 public ROOT =
+    bytes32 public root =
         0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
-    uint256 public AMOUNT_TO_CLAIM = 25 * 1e18;
-    uint256 public AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+    uint256 public amountToClaim = 25 * 1e18;
+    uint256 public amountToSend = amountToClaim * 4;
     bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
-    bytes32[] public PROOF = [proofOne, proofTwo];
+    bytes32[] public proof = [proofOne, proofTwo];
     address user;
     uint256 userPrivKey;
 
     function setUp() public {
         token = new BagelToken();
-        airdrop = new MerkleAirdrop(ROOT, token);
-        token.mint(address(airdrop), AMOUNT_TO_SEND);
+        airdrop = new MerkleAirdrop(root, token);
+        token.mint(address(airdrop), amountToSend);
         (user, userPrivKey) = makeAddrAndKey("user");
     }
 
     function testUsersCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
         vm.prank(user);
-        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        airdrop.claim(user, amountToClaim, proof);
 
         uint256 endingBalance = token.balanceOf(user);
-        assertEq(endingBalance, startingBalance + AMOUNT_TO_CLAIM);
+        assertEq(endingBalance, startingBalance + amountToClaim);
     }
 }
