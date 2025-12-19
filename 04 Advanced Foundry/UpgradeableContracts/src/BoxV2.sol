d6cd7d907dfcb13be799fe0aa1ea6386713ec9f2
Ariiellus <arielcardenas95@hotmail.com>
2025-12-19 15:17:14 -0600

    scripts

---
+++ 04 Advanced Foundry/UpgradeableContracts/src/BoxV2.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    error NotOwner();
+
+    uint256 internal number;
+
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+
+    function version() public pure returns (uint256) {
+        return 2;
+    }
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
