f30ba407695584ef30812b9c87be795a80426f8f
Ariiellus <arielcardenas95@hotmail.com>
2025-12-21 15:47:22 -0600

    adding AA folder

--- 04 Advanced Foundry/UpgradeableContracts/src/BoxV2.sol
+++ 04 Advanced Foundry/UpgradeableContracts/src/BoxV2.sol
@@ -2,10 +2,8 @@
 pragma solidity ^0.8.0;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract BoxV2 is UUPSUpgradeable {
 
     uint256 internal number;
 
